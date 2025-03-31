7823663a3ab85da1f0c41e913c75db5e20cead31
lopea <lopea5643@gmail.com>
2025-03-31 16:03:11 -0700

    s

---
+++ src/ImNodes.NET/Generated/ImNodesCol.gen.cs
@@ -0,0 +1,33 @@
+namespace imnodesNET
+{
+    public enum ImNodesCol
+    {
+        NodeBackground = 0,
+        NodeBackgroundHovered = 1,
+        NodeBackgroundSelected = 2,
+        NodeOutline = 3,
+        TitleBar = 4,
+        TitleBarHovered = 5,
+        TitleBarSelected = 6,
+        Link = 7,
+        LinkHovered = 8,
+        LinkSelected = 9,
+        Pin = 10,
+        PinHovered = 11,
+        BoxSelector = 12,
+        BoxSelectorOutline = 13,
+        GridBackground = 14,
+        GridLine = 15,
+        MiniMapBackground = 16,
+        MiniMapBackgroundHovered = 17,
+        MiniMapOutline = 18,
+        MiniMapOutlineHovered = 19,
+        MiniMapNodeBackground = 20,
+        MiniMapNodeBackgroundHovered = 21,
+        MiniMapNodeBackgroundSelected = 22,
+        MiniMapNodeOutline = 23,
+        MiniMapLink = 24,
+        MiniMapLinkSelected = 25,
+        COUNT = 26,
+    }
+}
